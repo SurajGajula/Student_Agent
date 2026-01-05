72cc0e1b109d0533f783f34d228d929e778d9565
SurajGajula <ssamgajula@gmail.com>
2026-01-05 14:26:29 -0800

    Cleanup debugging/files

--- rebase_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Create rebase todo file
-cat > /tmp/rebase_todo << 'EOF'
-edit a7b9e1b Railway Build Test
-pick da1ea2c fix: remove secrets from ecosystem.config.js.example
-EOF
-
-GIT_SEQUENCE_EDITOR="cp /tmp/rebase_todo" git rebase -i a7b9e1b~1
-
